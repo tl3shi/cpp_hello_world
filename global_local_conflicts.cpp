a0e2b50d45e11fa40c6c49915c44be7c90c14fea
tanglei <gems8@tanglei.name>
2014-10-21 17:12:52 +0800

    x

--- global_local_conflicts.cpp
+++ global_local_conflicts.cpp
@@ -9,19 +9,24 @@ struct G
     }
     G(int x):i(x)
     {
-        std::cout << "G(x)" << std::endl;
+        std::cout << "G(x)" << i << std::endl;
     }
     G(const G&g)
     {
         i = g.i;
         std::cout << "G(const G&g)"<< std::endl;
     }
+    G& operator = (const G &g)
+    {
+        i = g.i;
+        std::cout << "G & operator = (Const G &g)" << std::endl;
+    }
 };
-
 G g(1);
 int main(){
     int global = global;
     std::cout <<  "global=" << global << std::endl;
+    //G g;// = g;
     G g = g;
     std::cout << g.i << std::endl;
 }
